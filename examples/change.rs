d9c40b97df5f1f8f385a42b5d51077e8eb2f1910
alexanderjkslfj <117545308+alexanderjkslfj@users.noreply.github.com>
2024-08-24 15:42:22 +0200

    add example

---
+++ examples/change.rs
@@ -0,0 +1,38 @@
+use larix::Item;
+
+pub fn main() {
+    const XML: &str =
+        r#"<first><bob></bob><alice></alice></first>second<!-- third --><fourth happy="Yes" />"#;
+
+    // parse xml
+    let mut items = Item::from_str(XML).unwrap();
+
+    // remove last item
+    items.pop();
+
+    for item in &mut items {
+        match item {
+            Item::Element(element) => {
+                // add the attribute "happy", and set it to "Very much so"
+                element
+                    .attributes
+                    .insert(String::from("happy"), String::from("Very much so"));
+                // add a child called "peter"
+                element
+                    .children
+                    .push(Item::new_element(String::from("peter")));
+            }
+            Item::Text(text) => {
+                // change the text from "second" to " Hello "
+                text.replace_range(.., " Hello ");
+            }
+            Item::Comment(comment) => {
+                // change the comment from " third " to " World "
+                comment.replace_range(.., " World ");
+            }
+            _ => println!("Huh, that's odd."),
+        };
+    }
+
+    println!("{}", Item::to_str(&items));
+}
